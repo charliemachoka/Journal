1d4df15e092bca0f1701b597fef1a84b65a2de0e
carinaP2011 <carinahappuch@gmail.com>
2016-10-13 13:25:14 +0300

    feature delete

---
+++ delete_entry.py
@@ -0,0 +1,6 @@
+def delete_entry(entry):
+    """delete a journal created before"""
+
+    if input('You will not be able to view this again, are you sure? [y\/n]').upper() == 'y':
+        
+        print('Entry has been successfully deleted!')
